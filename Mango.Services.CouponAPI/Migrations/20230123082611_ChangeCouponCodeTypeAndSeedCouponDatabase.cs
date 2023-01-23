45d93435de026029ec45bb44c4f7fb6768c06e4b
Djengiz Ibrqmov <djengizibrqmov@gmail.com>
2023-01-23 10:32:19 +0200

    Implement CouponAPI

---
+++ Mango.Services.CouponAPI/Migrations/20230123082611_ChangeCouponCodeTypeAndSeedCouponDatabase.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    public partial class ChangeCouponCodeTypeAndSeedCouponDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "CouponCode",
+                table: "Coupons",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount" },
+                values: new object[] { 1, "10OFF", 10.0 });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount" },
+                values: new object[] { 2, "20OFF", 20.0 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CouponCode",
+                table: "Coupons",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.CouponAPI.Models.Dto
+{
+    public class CouponDto
+    {
+        public int CouponId { get; set; }
+        public int CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+    }
+}
